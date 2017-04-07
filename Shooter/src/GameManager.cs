69c428e343400c2f358f2fa64f3f2f138b5f8995
croquagei <andydavis0@hotmail.com>
2017-04-07 12:08:36 +1000

    end of class!

--- Shooter/src/GameManager.cs
+++ Shooter/src/GameManager.cs
@@ -14,7 +14,7 @@ namespace MyGame
 		//gameobject lists
 		private List<Bullet> _bullets;
 		private List<Enemy> _enemies;
-		//private Player _p1;
+		private Player _player;
 
 
 
@@ -35,14 +35,58 @@ namespace MyGame
 			Player p1 = new Player (2, SwinGame.LoadBitmap (@"sprites\F5S4-small.png"), 50f, 50f, new PeaShooter ());
 		}
 
+
+		//primary functions
+
+		public void Init () 
+		{
+			//do something?  maybe open the window?
+		}
+
 		public void HandleInput () 
 		{
 			//get the player to handle it's own input handle the players input
 		}
 
+		public void Update () 
+		{
+			//call all gameobjects to update
+
+
+
+
+		}
+
+
+
+		public void Render () 
+		{
+
+		}
+
+
+
+
+
+
+		//accessors
+
+		//using a method.  I don't what anything other than the world being able to do things with the enemy list.  When it comes to rendering i will have GameManager create a list of every Game object 
 		public void AddEnemy (Enemy e)
 		{
 			_enemies.Add (e);
+		}
+
+		public List<GameObject> GetAllGameObjects
+		{
+			get 
+			{
+				List<GameObject> allGameobjects = new List<GameObject> ();
+				allGameobjects.AddRange (_bullets);
+				allGameobjects.AddRange (_enemies);
+				allGameobjects.Add (_player);
+				return allGameobjects;
+			}
 		}
 
 
